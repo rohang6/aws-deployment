7883da51a48d2465dfea8cd74792a47f1a196fb3
rohanpg <rohangawarkar@gmail.com>
2025-09-23 20:11:33 +0530

    Initial Terraform + buildspec.yml

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+# s3 Bucket
+resource "aws_s3_bucket" "demo_bucket"{
+    bucket = "my-terraform-demo-bucket-12345"
+
+    tags = {
+        Environment = "Dev"
+        Project = "DemoPipeline"
+    }
+}
+
+# EC2 Instance 
+resource "aws_instance" "demo_ec2"{
+    ami = "ami-0f918f7e67a3323f0"
+    instance_type = "t2.micro"
+
+    tags = {
+        Name = "DemoEC2"
+    }   
+
+}
