795f4beea6f44536567bf08df365486f870b6d28
rohanpg <rohangawarkar@gmail.com>
2025-09-23 23:33:51 +0530

    buildspec for Destroy resources

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,9 @@
 output "s3_bucket_name" {
     description = "S3 bucket name"
-    value = aws_s3_bucket.demo_bucket
+    value = aws_s3_bucket.demo_bucket.bucket
 }
 
 output "ec2_public_ip" {
     description = "server public ip"
-    value = aws_instance.demo_ec2
+    value = aws_instance.demo_ec2.public_ip
 }
