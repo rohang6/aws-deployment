39e28d4b8ca3c768dafdbb782d8e347fcb9bc2d1
rohanpg <rohangawarkar@gmail.com>
2025-10-01 19:35:19 +0530

    Secrets manager name changed

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask
+import pymysql
+import os
+import boto3
+import json
+
+app = Flask(__name__)
+
+def get_db_creds():
+    secret_name = os.environ["DB_SECRET_ARN"]
+    region_name = os.environ["AWS_REGION"]
+    client = boto3.client("secretsmanager", region_name=region_name)
+    response = client.get_secret_value(SecretId=secret_name)
+    secret = json.loads(response["SecretString"])
+    return secret["username"], secret["password"]
+
+@app.route("/")
+def hello():
+    user, password = get_db_creds()
+    return f"Hello from Flask! Using DB user: {user}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
--- modules/rds/main.tf
+++ modules/rds/main.tf
@@ -33,7 +33,7 @@ resource "aws_db_instance" "mydb" {
 }
 
 resource "aws_secretsmanager_secret" "db_secret"{
-    name = "mydb_secret"
+    name = "mydb_secrets"
 }
 
 resource "aws_secretsmanager_secret_version" "db_secret_value" {
