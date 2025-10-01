07a0161bd3dc47396718557f50b3373ff722722e
rohanpg <rohangawarkar@gmail.com>
2025-10-01 09:50:59 +0530

    db_subnet_issue_fixed

--- main.tf
+++ main.tf
@@ -11,6 +11,6 @@ module "ec2" {
 module "rds" {
     source ="./modules/rds"
     vpc_id = module.vpc.vpc_id
-    subnets = module.vpc.private_subnet
+    subnets = module.vpc.private_subnet[0]
     ec2_sg_id = module.ec2.ec2_sg_id
 }
