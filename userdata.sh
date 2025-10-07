9361df5db240e5062e50f6681072ea5fbb829f38
rohanpg <rohangawarkar@gmail.com>
2025-10-07 08:57:35 +0530

    Updated

--- userdata.sh
+++ userdata.sh
@@ -9,3 +9,4 @@ sudo systemctl start docker
 usermod -aG docker ubuntu
 
 $(aws ecr get-login --no-include-email --region ap-south-1)
+docker run -d -p 80:5000 ${data.aws_ecr_reporitory.app.repository_url}:latest
