db65cefb0c18fbed0225b1a8da5bc5730a1b7fae
rohanpg <rohangawarkar@gmail.com>
2025-10-10 02:19:14 +0530

    Added Key

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -81,6 +81,7 @@ resource "aws_instance" "ubuntu" {
     subnet_id = var.subnet_id
     vpc_security_group_ids = [aws_security_group.ec2_sg.id]
     iam_instance_profile = aws_iam_instance_profile.ec2_access.name
+    key_name = "OtestKey.pem"
     tags = {
         Name = "myapp"
     }
